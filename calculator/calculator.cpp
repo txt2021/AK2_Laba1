6713570a0de69121e0eac1219ac1fd8b9efcc21d
txt2021 <47900977+txt2021@users.noreply.github.com>
2020-09-24 15:36:43 +0300

    Add files via upload

---
+++ calculator/calculator.cpp
@@ -0,0 +1,20 @@
+#include "calculator.h"
+
+int Calculator::Add (double a, double b)
+{
+<<<<<<< HEAD
+    return a + b;
+=======
+	return a + b + 0.5;
+>>>>>>> 4ad40a3... fix truncation error
+}
+
+int Calculator::Sub (double a, double b)
+{
+    return Add (a, -b);
+}
+
+int Calculator::Mul (double a, double b)
+{
+    return a * b + 0.5;
+}
---
+++ calculator/calculator.h
@@ -0,0 +1,16 @@
+#ifndef CALCULATOR_H
+#define CALCULATOR_H
+
+class Calculator
+{
+    public:
+<<<<<<< HEAD
+        int Add (double, double);
+        int Sub (double, double);
+=======
+        int Add (int, int);
+        int Mul (double, double);
+>>>>>>> e0ea21b... add a multiplication operation
+};
+
+#endif//CALCULATOR_H
